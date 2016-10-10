2bcc5a10276788d5e9673e3cfd32eff413d497db
hoford <hoford@google.com>
2016-10-10 15:56:32 -0700

    advanced chain test
    Change-Id: Iadb595b2fe7e08fd9205c967a69e850e1c585858

---
+++ solver/src/test/java/android/support/constraint/solver/AdvancedChainTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.support.constraint.solver;
+
+import android.support.constraint.solver.widgets.ConstraintAnchor;
+import android.support.constraint.solver.widgets.ConstraintWidget;
+import android.support.constraint.solver.widgets.ConstraintWidgetContainer;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+import static org.testng.Assert.assertEquals;
+
+public class AdvancedChainTest {
+
+    @Test
+    public void testSimpleHorizontalChainPacked() {
+        ConstraintWidgetContainer root = new ConstraintWidgetContainer(0, 0, 800, 600);
+        ConstraintWidget A = new ConstraintWidget(100, 20);
+        ConstraintWidget B = new ConstraintWidget(100, 20);
+
+        root.setDebugSolverName(root.getSystem(), "root");
+        A.setDebugSolverName(root.getSystem(), "A");
+        B.setDebugSolverName(root.getSystem(), "B");
+        ArrayList<ConstraintWidget> widgets = new ArrayList<>();
+        widgets.add(A);
+        widgets.add(B);
+        widgets.add(root);
+        root.add(A);
+        root.add(B);
+        A.connect(ConstraintAnchor.Type.TOP, root, ConstraintAnchor.Type.TOP, 20);
+        B.connect(ConstraintAnchor.Type.TOP, root, ConstraintAnchor.Type.TOP, 20);
+
+        A.connect(ConstraintAnchor.Type.LEFT, root, ConstraintAnchor.Type.LEFT, 0);
+        A.connect(ConstraintAnchor.Type.RIGHT, B, ConstraintAnchor.Type.LEFT, 0);
+        B.connect(ConstraintAnchor.Type.LEFT, A, ConstraintAnchor.Type.RIGHT, 0);
+        B.connect(ConstraintAnchor.Type.RIGHT, root, ConstraintAnchor.Type.RIGHT, 0);
+        A.setHorizontalChainPacked(true);
+        root.layout();
+        System.out.println("a) A: " + A + " B: " + B );
+        assertEquals(A.getLeft() - root.getLeft(), root.getRight() - B.getRight(), 1);
+        assertEquals(B.getLeft() - A.getRight(), 0, 1);
+    }
+
+    @Test
+    public void testSimpleVerticalTChainPacked() {
+        ConstraintWidgetContainer root = new ConstraintWidgetContainer(0, 0, 800, 600);
+        ConstraintWidget A = new ConstraintWidget(100, 20);
+        ConstraintWidget B = new ConstraintWidget(100, 20);
+
+        root.setDebugSolverName(root.getSystem(), "root");
+        A.setDebugSolverName(root.getSystem(), "A");
+        B.setDebugSolverName(root.getSystem(), "B");
+        ArrayList<ConstraintWidget> widgets = new ArrayList<>();
+        widgets.add(A);
+        widgets.add(B);
+        widgets.add(root);
+        root.add(A);
+        root.add(B);
+        A.connect(ConstraintAnchor.Type.LEFT, root, ConstraintAnchor.Type.LEFT, 20);
+        B.connect(ConstraintAnchor.Type.LEFT, root, ConstraintAnchor.Type.LEFT, 20);
+
+        A.connect(ConstraintAnchor.Type.TOP, root, ConstraintAnchor.Type.TOP, 0);
+        A.connect(ConstraintAnchor.Type.BOTTOM, B, ConstraintAnchor.Type.TOP, 0);
+        B.connect(ConstraintAnchor.Type.TOP, A, ConstraintAnchor.Type.BOTTOM, 0);
+        B.connect(ConstraintAnchor.Type.BOTTOM, root, ConstraintAnchor.Type.BOTTOM, 0);
+        A.setHorizontalChainPacked(true);
+        root.layout();
+        System.out.println("a) A: " + A + " B: " + B );
+        assertEquals(A.getTop() - root.getTop(), root.getBottom() - B.getBottom(), 1);
+        assertEquals(B.getTop() - A.getBottom(), 0, 1);
+    }
+
+}
