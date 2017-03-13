408ec97c02a06a232254e67b89a011c4cec3ef1c
Nicolas Roard <nicolasroard@google.com>
2017-03-13 12:24:55 -0700

    Set build version to 1.1.0
    Test: N/A
    
    Change-Id: I0991f8869fc0ee550ea833d05b8262ea188780b8

--- constraintlayout/src/main/java/android/support/constraint/ConstraintLayout.java
+++ constraintlayout/src/main/java/android/support/constraint/ConstraintLayout.java
@@ -388,7 +388,7 @@ public class ConstraintLayout extends ViewGroup {
     static final boolean ALLOWS_EMBEDDED = false;
 
     /** @hide */
-    public static final String VERSION="ConstraintLayout-1.0.0";
+    public static final String VERSION="ConstraintLayout-1.1.0";
     private static final String TAG = "ConstraintLayout";
     private static final boolean SIMPLE_LAYOUT = true;
 
