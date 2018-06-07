aaddafb4b7aedd31ae4b87d30fe43e09df77c5a6
Nicolas Roard <nicolasroard@google.com>
2018-06-06 18:12:32 -0700

    Move to 1.1.2
    Fixes: N/A
    Test: N/A
    
    Change-Id: I46a7fcf434010b9d3f4f05e3eca4cf05c03fe809

--- constraintlayout/src/main/java/android/support/constraint/ConstraintLayout.java
+++ constraintlayout/src/main/java/android/support/constraint/ConstraintLayout.java
@@ -480,7 +480,7 @@ public class ConstraintLayout extends ViewGroup {
     static final boolean ALLOWS_EMBEDDED = false;
 
     /** @hide */
-    public static final String VERSION = "ConstraintLayout-1.1.1";
+    public static final String VERSION = "ConstraintLayout-1.1.2";
     private static final String TAG = "ConstraintLayout";
 
     private static final boolean USE_CONSTRAINTS_HELPER = true;
